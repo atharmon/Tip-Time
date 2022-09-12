e446a14c08e2c80b2443cdb422130f3139e9c80d
atharmon <atharmon@tamu.edu>
2022-09-12 01:25:56 -0500

    Added 15% and 18% instrumentation tests

--- app/src/androidTest/kotlin/com/example/tiptime/CalculatorTests.kt
+++ app/src/androidTest/kotlin/com/example/tiptime/CalculatorTests.kt
@@ -28,5 +28,27 @@ class CalculatorTests {
 
         onView(withId(R.id.tip_result)).check(matches(withText(containsString("$10.00"))))
     }
+
+    @Test
+    fun calculate_18_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text)).perform(typeText("100.00")).perform(ViewActions.closeSoftKeyboard())
+
+        onView(withId(R.id.option_eighteen_percent)).perform(click())
+
+        onView(withId(R.id.calculate_button)).perform(click())
+
+        onView(withId(R.id.tip_result)).check(matches(withText(containsString("$18.00"))))
+    }
+
+    @Test
+    fun calculate_15_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text)).perform(typeText("100.00")).perform(ViewActions.closeSoftKeyboard())
+
+        onView(withId(R.id.option_eighteen_percent)).perform(click())
+
+        onView(withId(R.id.calculate_button)).perform(click())
+
+        onView(withId(R.id.tip_result)).check(matches(withText(containsString("$15.00"))))
+    }
 }
 
